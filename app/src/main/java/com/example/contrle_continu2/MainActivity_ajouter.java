d63441427c8f9b8a00511ed0402030491992dbe4
zaid bakkali <zaidj730@gmail.com>
2022-06-03 16:16:58 +0200

    7

---
+++ app/src/main/java/com/example/contrle_continu2/MainActivity_ajouter.java
@@ -0,0 +1,14 @@
+package com.example.contrle_continu2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_ajouter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_ajouter);
+    }
+}
---
+++ app/src/main/java/com/example/contrle_continu2/MainActivity_edite.java
@@ -0,0 +1,14 @@
+package com.example.contrle_continu2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_edite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_edite);
+    }
+}
