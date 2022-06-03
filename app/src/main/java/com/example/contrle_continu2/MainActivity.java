aaa65617daa777456afee75d472ac7ef9db3feac
zaid bakkali <zaidj730@gmail.com>
2022-06-03 16:30:37 +0200

    7 intent

--- app/src/main/java/com/example/contrle_continu2/MainActivity.java
+++ app/src/main/java/com/example/contrle_continu2/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.contrle_continu2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ajouters(View view) {
+
+        Intent i=new Intent(MainActivity.this,MainActivity_ajouter.class);;
+        startActivity(i);
+    }
+
+    public void editer(View view) {
+        Intent i=new Intent(MainActivity.this,MainActivity_edite.class);;
+        startActivity(i);
+    }
+
+    public void lister(View view) {
+        Intent i=new Intent(MainActivity.this,MainActivity_lister.class);;
+        startActivity(i);
     }
 }
