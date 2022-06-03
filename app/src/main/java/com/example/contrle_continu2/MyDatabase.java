78046f534949753bc7bbe21519b277194c5c9399
zaid bakkali <zaidj730@gmail.com>
2022-06-03 15:14:10 +0200

    4

--- app/src/main/java/com/example/contrle_continu2/MyDatabase.java
+++ app/src/main/java/com/example/contrle_continu2/MyDatabase.java
@@ -8,7 +8,7 @@ import androidx.annotation.Nullable;
 
 public class MyDatabase extends SQLiteOpenHelper {
 
-    public static String DB_NAME="DBProduit.db";
+    public static String DB_NAME="DBSociete.db";
     public static String TABLE_NAME="Societe";
     public static String COL1="ID";
     public static String COL2="Raison_Sociale";
@@ -23,11 +23,15 @@ public class MyDatabase extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sql = "create table " + TABLE_NAME + "("+COL1+" integer primary key autoincrement,"+COL2+" TEXT,"+COL3+" TEXT,"+COL4+" TEXT)";
+        sqLiteDatabase.execSQL(sql);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        String sql= "DROP TABLE " + TABLE_NAME;
+        sqLiteDatabase.execSQL(sql);
+        onCreate(sqLiteDatabase);
     }
 }
