08fce7bdae714b07d417fe91ab047ff0d51db8dd
zaid bakkali <zaidj730@gmail.com>
2022-06-03 16:44:08 +0200

    10 c

--- app/src/main/java/com/example/contrle_continu2/MainActivity_ajouter.java
+++ app/src/main/java/com/example/contrle_continu2/MainActivity_ajouter.java
@@ -3,12 +3,20 @@ package com.example.contrle_continu2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity_ajouter extends AppCompatActivity {
-
+    EditText nom_s,secteur,nb;
+    Button btn_eng,btn_anuler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_ajouter);
+    }
+
+    public void anuller(View view) {
+        finish();
     }
 }
